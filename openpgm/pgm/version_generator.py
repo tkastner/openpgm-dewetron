9bd1e33e04bcc2b0be1ded2b0a98807eeef1008a
Thomas Kastner <thomas.kastner@dewetron.com>
2017-03-03 11:54:59 +0100

    Fix typo in version generator

--- openpgm/pgm/version_generator.py
+++ openpgm/pgm/version_generator.py
@@ -4,14 +4,14 @@ import os
 import platform
 import time
 
-timestamp = time.gmtime(int(os.environ.get('SOURCE_DATE_EPOCH', time.time()))
+timestamp = time.gmtime(int(os.environ.get('SOURCE_DATE_EPOCH', time.time())))
 build_date = time.strftime ("%Y-%m-%d", timestamp)
 build_time = time.strftime ("%H:%M:%S", timestamp)
 build_rev = ''.join (list (filter (str.isdigit, "$Revision$")))
 
 print ("""
 /* vim:ts=8:sts=8:sw=4:noai:noexpandtab
- * 
+ *
  * OpenPGM version.
  *
  * Copyright (c) 2006-2014 Miru Limited.
@@ -20,12 +20,12 @@ print ("""
  * modify it under the terms of the GNU Lesser General Public
  * License as published by the Free Software Foundation; either
  * version 2.1 of the License, or (at your option) any later version.
- * 
+ *
  * This library is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
  * Lesser General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU Lesser General Public
  * License along with this library; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
